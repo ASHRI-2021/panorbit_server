371bc374f5652480022ad802372084cdc89a7d5c
ASHRI-2021 <aashrithbp1999@gmail.com>
2023-02-21 20:08:45 +0530

    new build

---
+++ build/precache-manifest.d54c3949fb9e30f43e6b33f52e513c0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "43312823bf4ec1323ac6",
+    "url": "/static/css/main.fed5e474.chunk.css"
+  },
+  {
+    "revision": "43312823bf4ec1323ac6",
+    "url": "/static/js/main.43312823.chunk.js"
+  },
+  {
+    "revision": "fdeb34be957d0625724f",
+    "url": "/static/js/1.fdeb34be.chunk.js"
+  },
+  {
+    "revision": "4e4a8f8ec3e08b23bfc0",
+    "url": "/static/js/2.4e4a8f8e.chunk.js"
+  },
+  {
+    "revision": "d8a50819be8bc712e0c2",
+    "url": "/static/js/runtime~main.d8a50819.js"
+  },
+  {
+    "revision": "a9533b88e5cab9130e9feebd5f150318",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7f23b87ff4b15ab40e3fb18949160b90.js"
+  "/precache-manifest.d54c3949fb9e30f43e6b33f52e513c0c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
